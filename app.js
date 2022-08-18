738b425c67ceea0b0a6dcfc1c35a1b23391c7d69
furkanssarri <furkansa@hotmail.com.tr>
2022-08-18 16:24:14 +0300

    Update the initial UI
    An incomplete version of the UI, where most of the code is broken and/or
    not responding as of yet. Will be updated in the future.

--- app.js
+++ app.js
@@ -1,22 +1,96 @@
-const container = document.querySelector(".container");
-const btn = document.querySelector("#generator");
+const canvas = document.querySelector(".canvas");
 const mainHeader = document.querySelector("#main-header");
+let mdown = false;
+let color = "#494e4c"
+let colorpicker = document.getElementById("colorpicker");
+let controllers = document.querySelector(".controllers");
 
-btn.addEventListener("click", promptUser);
 
-function promptUser() {
-   let userInput = prompt("Number?");
+
+eventListeners();
+
+function eventListeners() {
+   document.addEventListener("DOMContentLoaded", () => {
+      createGrid(33, 33);
+   });
+   // colorpicker.addEventListener("input", updateColor, false);
+   // colorpicker.addEventListener("change", watchColorPicker, false);
+   controllers.addEventListener("click", manageControls);
+}
+
+function appendRainbow(e) {
+   let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+   color = randomColor;
+}
+
+function manageControls(e) {
+   let newColor;
+   if (e.target.id === "clear") {
+      while (canvas.firstChild) {
+         canvas.firstChild.remove();
+      }
+   } else if (e.target.id === "colorpicker") {
+      updateColor(e);
+      colorpicker.addEventListener("change", () => {
+         watchColorPicker(e);
+         newColor = e.target.value;
+         console.log(color)
+         color = newColor;
+         return color;
+      });
+   } else if (e.target.id === "colorMode") {
+      
+      console.log(newColor);
+   } else if (e.target.id === "eraser") {
+      erase = "#fff";
+      color = erase;
+   }
+}
+
+function colorMode() {
+   let newColor = watchColorPicker(e);
+   console.log(newColor);
+   color = newColor;
+}
+
+// Color picker and update
+function watchColorPicker(e) {
+   newColor = e.target.value;
+   color = newColor;
+   return color;
+   
+}
+function updateColor(e) {
+   pickedColor = e.target.value;
+   color = pickedColor;
+   return color;
+}
+
+// Accept user input via data slider
+function showValue(newValue) {
+   document.getElementById("value").textContent = newValue;
+   let userInput = newValue;
    createGrid(userInput, userInput);
 }
 
+// Mouse down flag check
+["mousedown", "mouseup"].forEach(draw => canvas.addEventListener(draw, () => mdown = !mdown));
+
+// Mouse down control
+function cellEnter() {
+   if (mdown) {
+      this.style.backgroundColor = color;
+   }
+}
+
 function createGrid(userInput, userInput) {
-   while (container.firstChild) {
-      container.firstChild.remove();
+   while (canvas.firstChild) {
+      canvas.firstChild.remove();
    }
    if (userInput > 100) {
       let errorMessage = document.createElement("h2");
       errorMessage.textContent = "Please specify a number below 100.";
-      container.appendChild(errorMessage);
+      canvas.appendChild(errorMessage);
       return
    }
    for (let i = 0; i < userInput; i++) {
@@ -25,18 +99,14 @@ function createGrid(userInput, userInput) {
       for (let j = 0; j < userInput; j++) {
          let cell = document.createElement("div");
          cell.className = "grid";
-         cell.style.width = 100 / userInput + "vw";
-         cell.style.height = 100 / userInput + "vh";
-         let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-         cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = randomColor;
-         });
-         container.appendChild(cell);
-         mainHeader.remove();
+         cell.style.width = 100 / userInput + "%";
+         cell.style.height = 100 / userInput + "%";
+         cell.addEventListener("mouseenter", cellEnter);
+
+         canvas.appendChild(cell);
       }
    }
-
-
-
 }
+
+
 
